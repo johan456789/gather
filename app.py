9745c620d95f9cf785e2e9aed071086c7d529717
Tsung-Han Yu <14802181+johan456789@users.noreply.github.com>
2023-01-28 23:36:01 -0500

    cleanup comments

--- app.py
+++ app.py
@@ -9,26 +9,9 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql:///username:password@host:port/da
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-# TODO: create database
-''' (example)
-class Student(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    firstname = db.Column(db.String(100), nullable=False)
-    lastname = db.Column(db.String(100), nullable=False)
-    email = db.Column(db.String(80), unique=True, nullable=False)
-    age = db.Column(db.Integer)
-    created_at = db.Column(db.DateTime(timezone=True),
-                           server_default=func.now())
-    bio = db.Column(db.Text)
 
-    def __repr__(self):
-        return f'<Student {self.firstname}>'
-'''
-## TODO: USER TABLE
-'''
-USER
-id, name, email, password, contact
-'''
+# Database models
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
@@ -38,11 +21,6 @@ class User(db.Model):
     def __repr__(self):
         return f'<User {self.email}, {self.name}>'
 
-## TODO: PHOTO TABLE
-'''
-PHOTO
-email, upload_date, photo
-'''
 class Photo(db.Model):
     id = db.Column(db.Integer, primary_key=True)  # id for photo
     friend_name = db.Column(db.String(100), nullable=False)
