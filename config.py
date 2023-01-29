1b36a7e8a85a0f3cb49a645395f4859aed960481
Tsung-Han Yu <14802181+johan456789@users.noreply.github.com>
2023-01-28 23:41:06 -0500

    add config

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+from dotenv import load_dotenv, find_dotenv
+
+# this looks for a .env file in the directory and loads it up load_dotenv(find_dotenv())
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY')
