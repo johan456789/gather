29b68007141183a1ed75726a6838325816981cda
Tsung-Han Yu <14802181+johan456789@users.noreply.github.com>
2023-07-03 19:56:32 -0400

    add example sms script using twilio

--- send_sms.py
+++ send_sms.py
@@ -1,17 +1,24 @@
+import os
+from dotenv import load_dotenv, find_dotenv
 from twilio.rest import Client
-from config import Config
+
+
+TWILIO_ACCOUNT_SID = os.environ.get('TWILIO_ACCOUNT_SID')
+TWILIO_AUTH_TOKEN = os.environ.get('TWILIO_AUTH_TOKEN')
+EXAMPLE_PHOTO_URL = os.environ.get('EXAMPLE_PHOTO_URL')
+TO_PHONE_NUMBER = os.environ.get('TO_PHONE_NUMBER')
+FROM_PHONE_NUMBER = os.environ.get('FROM_PHONE_NUMBER')
 
 # Your Account SID from twilio.com/console
-account_sid = Config.TWILIO_ACCOUNT_SID
+account_sid = TWILIO_ACCOUNT_SID
 # Your Auth Token from twilio.com/console
-auth_token  = Config.TWILIO_AUTH_TOKEN
-photo_url = Config.EXAMPLE_PHOTO_URL
+auth_token = TWILIO_AUTH_TOKEN
+photo_url = EXAMPLE_PHOTO_URL
 
 client = Client(account_sid, auth_token)
-
 message = client.messages.create(
-            to="+11234567890", 
-            from_="+10987654321",
+            to=TO_PHONE_NUMBER, 
+            from_=FROM_PHONE_NUMBER,
             body="John wants to gather with you, again!")
             # media_url=photo_url)
 
